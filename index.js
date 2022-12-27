2da68d4d781251356073863454b19a5d32f8ca5e
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2022-12-27 19:41:46 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const sissors = document.getElementById("sissors")
+
+rock.addEventListener("click",()=> alert("rock"))
+paper.addEventListener("click",()=> alert("paper"))
+sissors.addEventListener("click",()=> alert("sissors"))
